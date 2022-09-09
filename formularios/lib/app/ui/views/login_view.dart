20979670541d4dad440b7f4b02aa4fac5888d336
Mauricio Montoya Medrano <mcubico33@gmail.com>
2022-09-09 06:23:39 -0500

    Agrego las validaciones al login

--- formularios/lib/app/ui/views/login_view.dart
+++ formularios/lib/app/ui/views/login_view.dart
@@ -17,8 +17,13 @@ class LoginView extends StatelessWidget {
                 child: Column(
                   children: [
                     const SizedBox(height: 10),
+
+                    // Title
                     Text('Login', style: Theme.of(context).textTheme.headline4),
+
                     const SizedBox(height: 30),
+
+                    // Form
                     const _LoginForm(),
                   ],
                 ),
@@ -42,50 +47,74 @@ class _LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // Email
-        TextFormField(
-          autocorrect: false,
-          keyboardType: TextInputType.emailAddress,
-          decoration: InputDecorationHelper.authInputDecoration(
-            hintText: 'joe.doe@email.com',
-            labelText: 'Correo Electrónico',
-            prefixIcon: Icons.alternate_email_rounded,
-          ),
-        ),
-        const SizedBox(height: 30),
-
-        // Password
-        TextFormField(
-          autocorrect: false,
-          obscureText: true,
-          keyboardType: TextInputType.emailAddress,
-          decoration: InputDecorationHelper.authInputDecoration(
-            hintText: '********',
-            labelText: 'Contraseña',
-            prefixIcon: Icons.lock_outline,
-          ),
-        ),
-        const SizedBox(height: 30),
-
-        // Submit
-        MaterialButton(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          disabledColor: Colors.grey,
-          elevation: 0,
-          color: Colors.deepPurple,
-          child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
-            child: Text(
-              'Ingresar',
-              style: TextStyle(color: Colors.white),
+    return Form(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      
+      child: Column(
+        children: [
+          // Email
+          TextFormField(
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecorationHelper.authInputDecoration(
+              hintText: 'joe.doe@email.com',
+              labelText: 'Correo Electrónico',
+              prefixIcon: Icons.alternate_email_rounded,
             ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Este campo es requerido';
+              }
+
+              String pattern =
+                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+              RegExp regExp = RegExp(pattern);
+
+              return regExp.hasMatch(value)
+                  ? null
+                  : 'El correo ingresado no es válido';
+            },
           ),
-          onPressed: () {},
-        ),
-      ],
+          const SizedBox(height: 30),
+
+          // Password
+          TextFormField(
+            autocorrect: false,
+            obscureText: true,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecorationHelper.authInputDecoration(
+              hintText: '********',
+              labelText: 'Contraseña',
+              prefixIcon: Icons.lock_outline,
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Este campo es requerido';
+              }
+
+              return null;
+            },
+          ),
+          const SizedBox(height: 30),
+
+          // Submit
+          MaterialButton(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            disabledColor: Colors.grey,
+            elevation: 0,
+            color: Colors.deepPurple,
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+              child: Text(
+                'Ingresar',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
     );
   }
 }
